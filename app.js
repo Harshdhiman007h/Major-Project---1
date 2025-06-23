494e7c6921180dce80fb2687880eabe2e49103f5
HarshDhiman007h <harshdeepdhiman78@gmail.com>
2025-06-23 16:20:25 +0530

    Moving toward The betterment of site

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderland";
 
@@ -20,6 +22,10 @@ async function main(){
 
 app.set("view engine" , "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/", (req, res)=>{
     res.send("Hi, I am Root");
@@ -30,8 +36,41 @@ app.get("/listings" ,async(req, res)=>{
     res.render("./listings/index.ejs", { allListings } );
 });
 
-app.get("/listings/:id" , (req,res)=>{
-    res.send("Working");
+app.get("/listings/new", (req, res)=>{
+    res.render("./listings/new.ejs");
+});
+
+app.post("/listings" ,async(req, res)=>{
+    let newlistings = new Listing(req.body.listing);
+    await newlistings.save();
+    res.redirect("/listings");
+});
+
+app.get("/listing/:id/edit", async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    if (!listing) {
+        return res.status(404).send('Listing not found');
+    }
+    res.render("./listings/edit.ejs",{ listing });
+});
+
+app.get("/listings/:id" , async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/show.ejs", {listing});
+});
+
+app.put("/listings/:id" , async(req , res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+app.get("/listing/:id/delete", async( req, res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
 });
 
 // app.get("/test", async(req, res)=>{
