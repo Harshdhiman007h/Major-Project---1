7e2859563d758955c4e56173442c1b7e14b1c5c8
Harshdhiman007h <harshdeepdhiman78@gmail.com>
2025-06-23 17:07:52 +0530

    Adding a New User Design

---
+++ listing.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listSchema = new Schema({
+    title: {
+        type:String,
+        required: true,
+    },
+    description:{
+        type: String,
+        default: "This is a Beautiful Site and Location is Perfect For Tourism",
+    },
+
+    image: {
+        filename: { type: String },
+        url: { type: String , default: "https://images.unsplash.com/photo-1518780664697-55e3ad937233?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",}
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing" , listSchema);
+
+module.exports = Listing;
